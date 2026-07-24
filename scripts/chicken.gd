a1db090c79f1312f99ebb360e908f2daef9495ae
SyntaxBrew <SyntaxBrew@outlook.com>
2026-07-23 17:22:42 -0700

    Add chicken spawning and movement

--- scripts/chicken.gd
+++ scripts/chicken.gd
@@ -1,7 +1,24 @@
 extends CharacterBody2D
+class_name Chicken
+
+@export var speed: float = 4
+
+var move_target
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	if move_target != null:
+
+		move_towards(move_target)
+	
+func move_towards(target_pos: Vector2) -> void:
+	var direction = (target_pos - self.position).normalized()
+	var new_velocity = direction * speed
+	
+	self.velocity = new_velocity
+	move_and_slide()
+
+func set_move_target(target_pos: Vector2) -> void:
+	move_target = target_pos
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,7 +1,32 @@
 extends Node2D
 
+const CHICKEN_SCENE = preload("res://scenes/chicken.tscn")
+
+@onready var chicken_spawns = $ChickenSpawns
+@onready var chicken_container = $Chickens
+@onready var base_marker: Marker2D = $BaseMarker
+
 func _ready() -> void:
-	pass
+	for wave in range(10):
+		for angle in range(0, 361, 10):
+			var radius: float = 100
+			var chicken_position: Vector2 = Vector2(
+				base_marker.position.x + radius * cos(deg_to_rad(angle)),
+				base_marker.position.y + radius * sin(deg_to_rad(angle))
+			) 
+			spawn_chicken(chicken_position)
+			await delay(0.1)
+		await delay(1)
 
 func _process(delta: float) -> void:
 	pass
+	
+func spawn_chicken(position: Vector2) -> void:
+	var chicken: Chicken = CHICKEN_SCENE.instantiate()
+	chicken.position = position
+	chicken.set_move_target(base_marker.position)
+	
+	chicken_container.add_child(chicken)
+
+func delay(seconds: float):
+	await get_tree().create_timer(seconds).timeout
